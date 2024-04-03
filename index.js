0e7cd0c13261a6f1fc8515fea89bfa8102c1c3c6
Tatiana Mathia <mathiatatiana@gmail.com>
2024-04-03 12:33:23 +0300

    loops

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+
+
+
+  
+
+
+function writeCards(namesArray, event){
+    let messages = []
+    for(let i =0; i < namesArray.length; i++){
+        messages.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages
+    
+}
+
+function countDown(number){
+    while(number > 0 ){
+        console.log(number);
+        number -= 1
+    }
+    console.log(number)
+}
